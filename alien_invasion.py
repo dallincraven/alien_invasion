75d925807dec6579578e71ade60e4074558f7bb7
Dallin Craven <52178169+dallincraven@users.noreply.github.com>
2025-06-13 12:15:44 -0600

    update alien_invasion.py & ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,7 +43,7 @@ class AlienInvasion:
                 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
-        self.screen.fill(self.setting.bg_color)
+        self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         
         pygame.display.flip()
